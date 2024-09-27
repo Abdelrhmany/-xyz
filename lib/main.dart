9f2cdef045e5058167ba249cac051a138894dfbf
Abdelrhmany <bdalrhmnyasr934@gmail.com>
2024-09-28 00:12:51 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:xyz/test/new.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Barcode Test',
+      debugShowCheckedModeBanner: false,
+      home: TestNewApp(),
+    );
+  }
+}
